95697dd6bfc6095980c67a6228782e31678a2433
Micca Osmar <mosmar@pop-os.localdomain>
2019-03-27 22:12:16 -0400

    Tutorial 04. Implemented exception filter and logging interceptor

---
+++ src/shared/http-error.filter.ts
@@ -0,0 +1,28 @@
+import { Catch, ExceptionFilter, HttpException, ArgumentsHost, Logger, HttpStatus } from '@nestjs/common';
+
+@Catch()
+export class HttpErrorFilter implements ExceptionFilter {
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const request = ctx.getRequest();
+        const response = ctx.getResponse();
+        const status = exception.getStatus ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+        const errorResponse = {
+            code: status,
+            timestamp: new Date().toLocaleString(),
+            path: request.url,
+            method: request.method,
+            message: exception.message.error || exception.message || null,
+        };
+
+        Logger.error(
+            `${request.method} ${request.url}`,
+            exception.stack,
+            'ExceptionFilter',
+        );
+
+        response.status(status).json(errorResponse);
+    }
+}
